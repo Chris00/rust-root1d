f3f4b72d87825b47dc7d2637f943c04c94114258
Christophe Troestler <Christophe.Troestler@umons.ac.be>
2022-04-08 12:54:11 +0200

    speed_rug: Print more information about the parameters

--- examples/speed_rug.rs
+++ examples/speed_rug.rs
@@ -10,6 +10,7 @@ fn main() -> Result<(), Box<dyn Error + Send + Sync + 'static>> {
     let mut sum = Float::with_val(prec, 0_f64);
 
     let n = 10;
+    let rtol = 1e-11f64;
     let a = Float::with_val(prec, 0_f64);
     let b = Float::with_val(prec, 100_f64);
     // Allocating the state out of the loop does not bring any
@@ -18,11 +19,9 @@ fn main() -> Result<(), Box<dyn Error + Send + Sync + 'static>> {
     // let mut r = a.clone();
     for _ in 0 .. 10_000 / n {
         for i in 2..100 {
-            // Trivial function so that it does not domainate the
-            // computation time.
             let c = Float::with_val(prec, i as f64);
             let f = |y: &mut Float, x: &Float| y.assign(x * x - &c);
-            let r = bisect_mut(f, &a, &b).atol(0f64).rtol(1e-10f64)
+            let r = bisect_mut(f, &a, &b).atol(0f64).rtol(rtol)
                 //.work(&mut w)
                 .root()?;
             // bisect_mut(f, &a, &b).atol(0f64).rtol(1e-10f64)
@@ -32,7 +31,8 @@ fn main() -> Result<(), Box<dyn Error + Send + Sync + 'static>> {
         }
     }
 
-    println!("Bisect<T=rug::Float> sum: {:.18} ({} times less iterations)",
-             sum, n);
+    println!("Bisect<T=rug::Float> sum: {:.18}", sum * n);
+    println!("    {} times less iterations, prec: {}, rtol: {:e}",
+             n, prec, rtol);
     Ok(())
 }
